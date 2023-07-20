4de828c2905cf4c5d012e45bc93340b24efd2b0c
Your Name <angeltcastillo11@gmail.com>
2023-07-19 19:18:17 -0500

    first commit

---
+++ orders.js
@@ -0,0 +1,25 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    document.getElementById("logout-btn").addEventListener("click", function() {
+        window.open("", "_blank");
+
+        window.location.href = "login.html";
+    }),
+]
